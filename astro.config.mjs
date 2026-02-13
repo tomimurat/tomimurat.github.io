7206d6d20540567a8df100bcea448c5bd886fa62
tomimurat <tomasmurat01@gmail.com>
2026-02-12 23:18:45 -0300

    Fix: Activando integración de Tailwind para procesar @apply

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
+  // Asegúrate de que este sea tu sitio exacto
   site: 'https://tomimurat.github.io',
+
   base: '/',
   integrations: [icon()],
-  // Esto obliga a Astro a meter todo el CSS junto para que no se pierda
+
   build: {
-    inlineStylesheets: 'always'
-  }
+    // ESTA ES LA CLAVE: Fuerza a Astro a no separar el CSS en archivos aparte.
+    // Lo inyectará directamente en el HTML para que no haya pérdida.
+    inlineStylesheets: 'always',
+  },
+
+  // Evita que Astro genere rutas que confundan a GitHub Pages
+  compressHTML: true,
+
+  outDir: 'dist',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
