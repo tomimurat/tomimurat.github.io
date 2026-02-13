c5a2fae0e3d5cd3600d72cca29f0bc42897f578e
tomimurat <tomasmurat01@gmail.com>
2026-02-12 22:36:59 -0300

    Fix: resolve astro-icon virtual import error

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://tomimurat.github.io',
   base: '/', // Solo si el repo NO se llama tu-usuario.github.io
+  integrations: [
+    icon(), // Debe estar inicializado como una función
+  ],
 });
